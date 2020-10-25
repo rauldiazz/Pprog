6a02a3e9498790f9cccff5b9d51426674d0ba42c
elpeonkamilkaze <elpeonkamikaze@gmail.com>
2020-10-25 12:55:17 +0100

    peones

--- movimientos.c
+++ movimientos.c
@@ -3,6 +3,7 @@
 
 #define PEONDIR 10
 
+
 MOVE *create_move(){
     return (MOVE*)malloc(sizeof(MOVE));
 }
@@ -10,7 +11,16 @@ MOVE *create_move(){
 void free_move(MOVE *m){
     free(m);
 }
-
+MOVE insert_move(int castle, int from, int to, int pieza, int captura, int corona, int paso){
+    MOVE m;
+    m.castle = castle;
+    m.from = from;
+    m.piezas[0] = pieza;
+    m.piezas[1] = captura;
+    m.piezas[2] = corona;
+    m.paso = paso;
+    return m;
+}
 
 MOVE *Generador_Movimientos(TABLERO *t){
     int count = 0;
@@ -22,19 +32,82 @@ MOVE *Generador_Movimientos(TABLERO *t){
     
 } 
 
+MOVE * AddMovePeon (MOVE *m,  int *count, int cas, int to, int to2, int captura, int side, int paso){
+    if (!m) return NULL;
+    if(Cas_Fila(cas) == FILA_7 - side*5){
+        m = realloc(m,*count + 4);
+        if (!m) return NULL;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, CAMBIO_LADO*side + wN, paso);
+        *count ++;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, CAMBIO_LADO*side + wB, paso);
+        *count ++;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, CAMBIO_LADO*side + wR, paso);
+        *count ++;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, CAMBIO_LADO*side + wQ, paso);
+        *count ++;
+    }
+    else if (Cas_Fila(cas) == FILA_2 +side*5 && captura == EMPTY && to2 == EMPTY){
+        m = realloc(m, *count + 2);
+        if (!m) return NULL;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, EMPTY, paso);
+        *count++;
+        m[*count] = insert_move(EMPTY,cas, to + 10 - 20*side,CAMBIO_LADO*side + wP, captura, EMPTY, paso);
+        count++;
+    }
+    else{
+        m = realloc(m, *count +1);
+        if (!m) return NULL;
+        m[*count] = insert_move(EMPTY,cas, to,CAMBIO_LADO*side + wP, captura, EMPTY, paso);
+        *count++;
+    }
+    return m;
+}
+
 
 MOVE * Generador_Peones(TABLERO *t, MOVE *m, int *count ){
+    MOVE mt;
     int i;
     int side;
     int cas = 0, cas_temp=0;
     int pieza = EMPTY;
+    short flag = 1;
 
     if(!m || !t) return -1;
 
     side = t->side;
 
-    for(i=0;i<t->pceNum[CAMBIO_LADO*side + wP]){
-        cas = 
-    }
+    //Las correciones donde hay *side sirven para escoger, dependiendo del lado al que le toque, escoger las características correctas
+    for(i=0;i<t->pceNum[CAMBIO_LADO*side + wP]&& flag == 1;i++){
+        cas = t->pList[CAMBIO_LADO*side + wP][i];
+        ASSERT(cas != OFFBOARD)
 
+        if(t->pieces[cas + 10 - 20*side] == EMPTY){
+            m = AddMovePeon(m, count, cas, cas + 10 - 20*side, t->pieces[cas + 20 - 40*side],EMPTY,side,EMPTY);
+            if (!m) flag = 0;
+        }
+        
+        if(t->pieces[cas +11 - 20*side] != EMPTY && flag == 1){
+            m = AddMovePeon(m,count, cas, cas +11 - 20*side, EMPTY,t->pieces[cas +11 - 20*side], side, EMPTY);
+            if (!m) flag = 0;
+        }
+
+        if (t->pieces[cas +9 - 20*side] != EMPTY && flag == 1){
+            m = AddMovePeon(m,count, cas, cas +9 - 20*side, EMPTY,t->pieces[cas +9 - 20*side], side, EMPTY);
+            if (!m) flag = 0;
+        }
+
+        if (t->AlPaso == cas +9 - 20*side && flag == 1){
+            m = AddMovePeon(m,count, cas, cas +9 - 20*side, EMPTY,-(CAMBIO_LADO*side) + bP, side, 1);
+            if (!m) flag = 0;
+        }
+        else if (t->AlPaso == cas +11 - 20*side && flag == 1){
+            m = AddMovePeon(m,count, cas, cas +11 - 20*side, EMPTY,-(CAMBIO_LADO*side) + bP, side, 1);
+            if (!m) flag = 0;
+        }
+        
+    }
+    if (flag == 0){
+        return NULL;
+    }
+    return m;
 }
