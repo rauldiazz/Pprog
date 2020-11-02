87dba9fe0891b377f4bfe37b797f96ba0537260d
Sergioleal14 <lealandressergio@gmail.com>
2020-11-02 16:12:27 +0100

    a

--- main.c
+++ main.c
@@ -2,39 +2,25 @@
 #include "definiciones.h"
 #include "stdlib.h"
 
-#define FEN1 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-#define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-#define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
-#define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
-
-
+#define PERFTFEN "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 int main() {	
 
-TABLERO *tab=NULL;
-FILE *pf;
-int i=0;
-char *aux;
+	TABLERO *tab=NULL;
+
+	tab=Create_tablero();
+	InitFILAsCOLsBrd();
+
+	LeerFen(PERFTFEN, tab);
 
 
-tab=Create_tablero();
-InitFILAsCOLsBrd();
-//pf=fopen("r", "prueba.txt");
-LeerFen(FEN1, tab);
-CheckBoard(tab);
-PrintBoard(tab);
 
-/*
-for(i=0;i<4;i++){
+	Comprobaciontest(3,tab);
 
-fscanf(pf,"%s\n", aux);
-LeerFen(aux, tab);
-CheckBoard(tab);
-PrintBoard(tab);
 
-}
-*/
-Free_tablero(tab);
-
+	Free_tablero(tab);
 	
+	
+
+
 	return 0;
 }
