2202b9aca808487d96f0eeaa664d8a26cb8631fc
Sergioleal14 <lealandressergio@gmail.com>
2020-11-24 19:58:14 +0100

    a

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "definiciones.h"
 #include "stdlib.h"
 
-#define FEN1 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-#define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+#define FEN1 "7k/6pp/8/8/8/8/3R1K2/8 w  - - 0 1"
+#define FEN2 "5rk1/4bppp/4p3/8/p3PP2/3K3R/8/8 w  - - 0 1"
 #define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 #define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 1"
 #define FEN5 "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P5/RNBQKBNR w KQkq e6 0 1"
@@ -16,6 +16,7 @@
 #define FENAUX2 "2k5/7Q/R7/2p5/8/8/1PPP1PBP/2B1K2R w K - 0 1"
 #define FENAUX "r3k2r/ppp2pQp/2n5/3n4/8/8/RPPP1PBP/2B1K2R w Kkq - 0 1"
 #define FENAB "6k1/R4p2/2r4p/3r2P1/5P2/3P4/6P1/3R2K1 w  - - 0 1"
+
 int main() {	
 
 	TABLERO *tab=NULL;
@@ -31,10 +32,10 @@ int main() {
 	tab=Create_tablero();
 	InitFILAsCOLsBrd();
 
-	LeerFen(START_FEN, tab);
+	LeerFen(FEN1, tab);
 
 	Menu_juego(tab);
-
+	Free_tablero(tab);
 
 	/*PrintBoard(tab);
 
