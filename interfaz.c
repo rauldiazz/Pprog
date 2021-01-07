9343f7436e1560b8bbc8bfb27f113b43d1415cbb
elpeonkamilkaze <elpeonkamikaze@gmail.com>
2021-01-07 16:41:58 +0100

    sinprintfs

--- interfaz.c
+++ interfaz.c
@@ -92,7 +92,7 @@ MOVE *LeerMovimiento(char *entrada, TABLERO *t){
             return NULL;
         }
         else if(strncmp("0-0",aux,3) == 0){
-            //printf("Esoty en enroque\n");
+
             m=insert_move(WKCA + t->side*3, EMPTY,EMPTY,EMPTY, EMPTY,EMPTY, EMPTY);
             if(is_Valid(m,t)) return m;
             free_move(m);
@@ -102,7 +102,7 @@ MOVE *LeerMovimiento(char *entrada, TABLERO *t){
     }
     else if (*aux >= 'a' && *aux <= 'h'){
         pieza = wP +t->side*(CAMBIO_LADO);
-      //  printf("es una jugada de peon\n");
+
     }
 
     switch (*aux)
@@ -117,40 +117,40 @@ MOVE *LeerMovimiento(char *entrada, TABLERO *t){
             break;
     }
 
-    //printf("random printf,pieza es %d\n",pieza);
+
     if(pieza == EMPTY) return NULL;
     if ((*aux) < 'a' || (*aux) > 'h') return NULL;
     if(*(aux+1) < '1' || *(aux+1) > '8') return NULL;
 
-  //  printf("la columna es %d, y la fila es %d, FECAS es %d\n",*aux-'a',*(aux+1)-'1', FCCAS(0,1));
+
     from = FCCAS(*aux-'a',*(aux+1)-'1');
     aux++;
-  //  printf("from es %d, a2 es %d\n",from,A2);
+
     if(from < 0 || from >= 120) return NULL;
     if(t->pieces[from] == OFFBOARD) return NULL;
     aux++;
-    //printf("Ha pasado from\n");
+
     if(*aux == 'x'){
         captura = 1;
         aux++;
     }
-    //printf(" aux1   %d      \n", *aux);
+
     if ((*aux) < 'a' || (*aux) > 'h') return NULL;
-    //printf("aux2    %d      \n", *aux);
+
     
     if(*(aux+1) < '1' || *(aux+1) > '8') return NULL;
-    //printf("antes de FCCAS\n");
+
     to = FCCAS(*aux-'a', *(aux+1) -'1');
     aux++;
     if(to < 0 || to >= 120) return NULL;
-    //printf("Ha pasdo to\n");
+
     if(t->pieces[to] == OFFBOARD) return NULL;
     aux++;
-    //printf("Antes de switch\n");
+
     if(*aux == '=' && pieza ==  wP +t->side*(CAMBIO_LADO)){
-        //printf("Hay una coronacion\n");
+
         aux++;
-        //printf("corono a: %c ",*aux);
+
         switch (*aux){
             case 'N': corona = wN + CAMBIO_LADO*(t->side); break;
             case 'B': corona = wB + CAMBIO_LADO*(t->side); break;
@@ -166,11 +166,11 @@ MOVE *LeerMovimiento(char *entrada, TABLERO *t){
             paso = to;
         }
     }
-    //printf("Antes de insertmove\n");
+
     m = insert_move(EMPTY, from,to,pieza, captura,corona, paso);
-    //PrintMove(m);
+
     if(is_Valid(m,t)){
-    //    printf("Valid es %d\n", is_Valid(m,t));
+
         
         return m;
     }
@@ -202,7 +202,7 @@ int ObtenerJugada(TABLERO *tab){
         printf("Introduzca su jugada:\n");
         fflush(stdin);
         if(fgets(entradajugada, sizeof(entradajugada),stdin) == NULL) flag = ERR;
-        //printf("Tu jugada ha sido:%s;\n",entradajugada);
+
         if(flag != ERR){
             if(strncmp(entradajugada,"exit",4)==0){
                 flag = EXIT;
